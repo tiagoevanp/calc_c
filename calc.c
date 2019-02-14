81da9fd41f337ad2544d68893301ae1417361dbd
Tiago Evangelista Pinto <tiagoevan.canopus@terra.com.br>
2019-02-14 18:43:52 -0200

    simple calc funcionando

--- calc.c
+++ calc.c
@@ -1,35 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long calc(long num1, long num2, char operator) {
-	printf("%ln\n", num1);
+long calc(long num1, long num2, char* operator) {
 	long result;
 
-	// switch(operator) {
-	// 	case '+' : result = num1 + num2; break;
-	// 	case '-': result = num1 - num2; break;
-	// 	case '*': result = num1 * num2; break;
-	// 	case '/': result = num1 / num2; break;
-	// }
+	switch(operator[0]) {
+		case '+' : result = num1 + num2; break;
+		case '-' : result = num1 - num2; break;
+		case 'x' : result = num1 * num2; break;
+		case '/' : result = num1 / num2; break;
+	}
 	return result;
 }
 
-void main(int argcs, char **argv) {
-	long *firstNum;
-	long *secondNum;
-	long *result;
-	char *operator;
+void main(int argc, char **argv) {
+	char * a;
 
-	firstNum = malloc(sizeof(long));
-	secondNum = malloc(sizeof(long));
-	result = malloc(sizeof(long));
-	operator = malloc(sizeof(char));
+	long firstNum = strtol(argv[1], &a , 10);
+	long secondNum = strtol(argv[3], &a , 10);
 	
-	*result = calc(*argv[1], *argv[3], *argv[2]);
-	printf("%ln\n", result);
-
-	free(firstNum);
-	free(secondNum);
-	free(operator);
-	free(result);
+	long result = calc(firstNum, secondNum, argv[2]);
+	printf("%ld\n", result);
 }
