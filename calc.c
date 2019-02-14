35f2c193c175981bd11775aa1de9c448339637ba
Tiago Evangelista Pinto <tiagoevan.canopus@terra.com.br>
2019-02-14 10:06:38 -0200

    init

--- calc.c
+++ calc.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() {
-	char * nome;
+long calc(long num1, long num2, char operator) {
+	printf("%ln\n", num1);
+	long result;
 
-	nome = malloc(sizeof(char) * 20);
+	// switch(operator) {
+	// 	case '+' : result = num1 + num2; break;
+	// 	case '-': result = num1 - num2; break;
+	// 	case '*': result = num1 * num2; break;
+	// 	case '/': result = num1 / num2; break;
+	// }
+	return result;
+}
 
-	printf("Digite seu nome: ");
-	scanf("%[^\n]", nome);
+void main(int argcs, char **argv) {
+	long *firstNum;
+	long *secondNum;
+	long *result;
+	char *operator;
+
+	firstNum = malloc(sizeof(long));
+	secondNum = malloc(sizeof(long));
+	result = malloc(sizeof(long));
+	operator = malloc(sizeof(char));
 	
-	printf("Seu nome é: %s \n", nome);
+	*result = calc(*argv[1], *argv[3], *argv[2]);
+	printf("%ln\n", result);
 
-	free(nome);
-
+	free(firstNum);
+	free(secondNum);
+	free(operator);
+	free(result);
 }
